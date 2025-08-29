f899f4ff5f9e3271cf6843855fb5e894497c2114
Zidane-ETML <pd01wvj@eduvaud.ch>
2025-08-29 11:11:19 +0200

    feat(Exercice snail procédural)

---
+++ personnel/snail/exercice/Snail.Zidane/Snail.Zidane/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            string snail = "_@_ö";
+            int position = 0;
+            for (int i = 50; i > 0; i--)
+            {
+                Thread.Sleep(50);
+                Console.Clear();
+                Console.SetCursorPosition(position, 0);
+                Console.WriteLine(snail);
+                position++;
+            }
+            Console.Clear();
+            Console.SetCursorPosition(position, 0);
+            Console.WriteLine("____");
+            Console.ReadLine();
+        }
+    }
+}

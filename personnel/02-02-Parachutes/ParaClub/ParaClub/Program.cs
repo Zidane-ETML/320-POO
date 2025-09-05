25ebd404abaedf46802427d5b0f7024edbac4f45
Zidane-ETML <pd01wvj@eduvaud.ch>
2025-09-05 09:07:28 +0200

    feat(exo parachute) : Ajout de la classe config et de la dimension de l'écran via celle-ci

---
+++ personnel/02-02-Parachutes/ParaClub/ParaClub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+            Console.WriteLine("Hello World!");
+        }
+        static class Config
+        {
+            public const int SCREEN_HEIGHT = 40;
+            public const int SCREEN_WIDTH = 150;
+        }
+    }
+}

16c745183aeadebe0aafc8af146e132aa3929946
Zidane-ETML <pd01wvj@eduvaud.ch>
2025-10-03 10:46:05 +0200

    chore(drone): Création de la classe Box

--- personnel/Drones/Drones/Drones/Program.cs
+++ personnel/Drones/Drones/Drones/Program.cs
@@ -18,7 +18,7 @@ namespace Drones
             List<Drone> fleet= new List<Drone>();
             for (int i = 0; i < 6; i++)
             {
-                string name = "kiks'miousik";
+                string name = "Drone";
                 string namei = name + i;
                 Drone drone = new Drone(i * 10, i * 100, namei);
                 fleet.Add(drone);
